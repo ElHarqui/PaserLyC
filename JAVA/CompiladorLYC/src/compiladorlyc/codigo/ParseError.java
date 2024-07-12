f8b4c8a4bc6e8399fb4a362ebf0d0a5fd4497fad
Sebastian <sebastian.peralta@unmsm.edu.pe>
2024-07-12 11:28:35 -0500

    Aa

--- JAVA/CompiladorLYC/src/compiladorlyc/codigo/ParseError.java
+++
@@ -1,7 +0,0 @@
-package compiladorlyc.codigo;
-
-public class ParseError extends RuntimeException {
-    public ParseError(String message) {
-        super(message);
-    }
-}
--- JAVA/CompiladorLYC/src/compiladorlyc/codigo/TokenType.java
+++
@@ -1,6 +0,0 @@
-
-package compiladorlyc.codigo;
-
-public enum TokenType {
-    INTEGER, FLOAT, IDENTIFIER, OPERATOR, SEMICOLON, END_OF_FILE, UNKNOWN
-}
